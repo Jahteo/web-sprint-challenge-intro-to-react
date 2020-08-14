207bb9f0badc4ccf5a98b3db6656366e84dd8e6c
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-14 10:48:20 -0700

    some styled components added.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,32 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios"
+import styled from "styled-components"
 import './App.css';
-// import makeCharacterCards from "./components/MakeCharacterCards"
 import Character from "./components/Character"
 
-const App = () => {
+const StyledDetails = styled.div`
+/* * {border: solid black 1px;} */
 
+   display: flex;
+   flex-wrap: wrap;
+   /* flex-direction: column; */
+
+`
+
+const App = () => {
   const [charactersList, setCharactersList] = useState([])
 
   useEffect(() => {
     axios.get("https://swapi.dev/api/people/")
     .then(res => {
       setCharactersList(res.data.results)
-      console.log("results:", res.data)
+      // console.log("results:", res.data.results)
     })
-    .catch(err => {
-      console.log(err)
-    })
-    // return () => {does it make sense to clean up?}
+    .catch(err => {console.log(err)})
   }, [])
 
-  // console.log("charList:", charactersList)
+  console.log("charList:", charactersList)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <StyledDetails >
       {charactersList.map(char => {
-        return <Character key={char} />
+        return <Character key={char.name} char={char} />
       })}
+      </StyledDetails>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,17 +1,25 @@
 import React from "react"
+import styled from "styled-components"
 
-const Character = () => {
+const StyledDetails = styled.div`
+    border: solid black 1px;
+    /* display: flex;
+    justify-content: space-evenly; */
+    margin: 2% auto;
+    padding: 2%;
+    h1 {
+
+    }
+`
 
 
+const Character = ({char}) => {
 
-
-
-console.log("Character")
     return (
-        <div>
-            <h2>Name</h2>
-            <p>height:mass (ratio)</p>
-        </div>
+        <StyledDetails>
+            <h2>Name: {char.name}</h2>
+            <p>height:mass (ratio) = {char.height}:{char.mass} ({(char.height / char.mass).toPrecision(4)}) </p>
+        </StyledDetails>
 
     )
 }
